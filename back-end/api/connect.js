0382c596c471cc7346a844d6fba27425f8cf3303
M-eduardabronzoni <bronzonieduarda@gmail.com>
2025-02-16 20:46:04 -0300

    ajustes para o deploy

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -11,4 +11,4 @@ const client = new MongoClient(URI);
 export const db = client.db("spotifyAula");
 const songCollection = await db.collection("songs").find({}).toArray();
 
-console.log(songCollection);
+// console.log(songCollection);
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,10 @@
 // Fetch ou Axios - consome api 
+import "dotenv/config";
 import axios from "axios";
 
+const {NODE_ENV} = process.env;
 // Endpoint acessado para fazer as requisições
-const URL = "http://localhost:3001"; 
+const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api"; 
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
